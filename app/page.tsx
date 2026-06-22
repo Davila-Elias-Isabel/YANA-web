930774ab9a81b08f5657bb59b1c0245668225af5
Davila Elias Isabel <rociodavilaelias@gmail.com>
2026-06-22 17:36:52 -0500

    Clean up Home component by removing unused imports
    Removed unused imports and components from Home function.

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,6 @@
 'use client'
 
-import { useState } from 'react'
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import Features from '@/components/Features'
-import HowItWorks from '@/components/HowItWorks'
-import CTA from '@/components/CTA'
-import Footer from '@/components/Footer'
-
-export default function Home() {
-  return (
-    <main>
-      <Header />
-      <Hero />
-      <Features />
-      <HowItWorks />
-      <CTA />
-      <Footer />
-    </main>
-  )
-}
+// ... resto del archivo
